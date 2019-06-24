cc0976c1523ba54e86ee17a6f094126e31298f20
Jonathon Chambers <jonathon@jonathonchambers.com>
2019-06-24 13:17:37 -0400

    Added all test cases to Test File

--- pushover_test.py
+++ pushover_test.py
@@ -1,8 +1,9 @@
 import unittest
 import os
-from pushover import PushoverClient
+from pushover import PushoverClient, PushoverPriority
 
 class TestPushoverClient(unittest.TestCase):
+
     def setUp(self):
         self.APP_KEY = os.environ['PUSHOVER_APP_KEY']
         self.TEST_USER = os.environ['TEST_USER_KEY']
@@ -13,7 +14,27 @@ class TestPushoverClient(unittest.TestCase):
 
         assert (self.TEST_USER != "") and (self.APP_KEY !="")
         self.client = PushoverClient(self.APP_KEY)
+
     def test_MessagePlain(self):
         assert(self.client.send_message(self.TEST_USER, self.MESSAGE) is True)
+
+    def test_Message_With_Title(self):
+        assert(self.client.send_message(self.TEST_USER, self.MESSAGE, "TEST TITLE") is True)
+
+    def test_NOALERT_Message(self):
+        assert(self.client.send_message(self.TEST_USER, self.MESSAGE, 
+                                        priority = PushoverPriority.NOALERT) is True)
+
+    def test_QUIET_Message(self):
+        assert(self.client.send_message(self.TEST_USER, self.MESSAGE, 
+                                        priority = PushoverPriority.QUIET) is True)  
+
+    def test_HIGH_Message(self):
+        assert(self.client.send_message(self.TEST_USER, self.MESSAGE, 
+                                        priority = PushoverPriority.HIGH) is True)
+                            
+    def test_CONFIRM_NEEDED_Message(self):
+        assert(self.client.send_message(self.TEST_USER, self.MESSAGE, 
+                                        priority = PushoverPriority.CONFIRM_NEEDED) is True)
 if __name__ == "__main__":
     unittest.main()
