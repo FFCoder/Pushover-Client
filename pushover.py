aed9d4c9cd95daf1cdde6d1cc0895df16e358ffe
Jonathon Chambers <jonathon@jonathonchambers.com>
2019-06-24 13:30:35 -0400

    Updated Pushover Module to handle high priority messages

--- pushover.py
+++ pushover.py
@@ -31,8 +31,14 @@ class PushoverClient:
         self.log = logging.getLogger("com.jonathonchambers.pushoverclient")
 
 
-    def send_message(self, user: str, message: str, title: str = None, 
-                     priority: PushoverPriority = None):
+    def send_message(self, 
+                     user: str,
+                     message: str, 
+                     title: str = None, 
+                     priority: PushoverPriority = None,
+                     retry: int = 60,
+                     expire: int = 180
+                     ):
         """ Sends a message to a user.
         Parameters
         ----------
@@ -43,20 +49,33 @@ class PushoverClient:
         title : str, 
             Per PushoverAPI Docs: your message's title, otherwise your app's name is used 
         priority: PushoverPriority
-            The Pushover Priority of the message
+            The Pushover Priority of the message -- Determines how the message is displayed.
+        retry : int, 
+            Per PushoverAPI Docs: The retry parameter specifies how often (in seconds) 
+            the Pushover servers will send the same notification to the user.
+        expire : int,
+            Per PushoverAPI Docs: The expire parameter specifies how many seconds your 
+            notification will continue to be retried for (every retry seconds). 
+            If the notification has not been acknowledged in expire seconds, 
+            it will be marked as expired and will stop being sent to the user. 
+            Note that the notification is still shown to the user after it is expired, 
+            but it will not prompt the user for acknowledgement. This parameter must 
+            have a maximum value of at most 10800 seconds (3 hours). 
         """
         
         # Setup a dict for Pushover Variables
-        d = {
+        request_data = {
             "token": self.token,
             "user": user,
             "message": message,
             "title": title,
-            "priority": priority
+            "priority": priority,
+            "expire": expire,
+            "retry": retry
         }
         self.log.debug("Data Variable: ")
-        self.log.debug(d)
-        req = requests.post(PushoverClient.API_ENDPOINT, data = d)
+        self.log.debug(request_data)
+        req = requests.post(PushoverClient.API_ENDPOINT, data = request_data)
 
         if ((req.status_code == 200) or (req.status_code == 201)):
             return True
